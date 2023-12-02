3fc23de92e8fc96c1217d183275dbc8c6d7aafcc
aapozd <aapozdnyakove@gmail.com>
2023-12-02 11:53:09 +0000

    Update mailing ui
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- dormyboba/handlers/mailing.py
+++ dormyboba/handlers/mailing.py
@@ -11,6 +11,22 @@ class MailingState(BaseStateGroup):
     PENDING_DATE = "pending_date"
     PENDING_TIME = "pending_time"
 
+KEYBOARD_MAILING = (
+    Keyboard()
+    .add(Text("Задать тему рассылки", payload={"command": "mailing_theme"}))
+    .row()
+    .add(Text("Задать текст рассылки", payload={"command": "mailing_text"}))
+    .row()
+    .add(Text("Задать дату рассылки", payload={"command": "mailing_date"}))
+    .row()
+    .add(Text("Задать время рассылки", payload={"command": "mailing_time"}))
+    .row()
+    .add(Text("Готово", payload={"command": "mailing_done"}))
+    .row()
+    .add(Text("Назад", payload={"command": "help"}))
+    .get_json()
+)
+
 @mailing_labeler.message(command="forward")
 async def forward(message: Message) -> None:
     await message.answer("Сообщение успешно отправлено")
@@ -21,25 +37,48 @@ async def forward_imp(message: Message) -> None:
 
 @mailing_labeler.message(payload={"command": "mailing"})
 async def mailing(message: Message) -> None:
+    await message.answer("Начат процесс создания рассылки", keyboard=KEYBOARD_MAILING)
+
+@mailing_labeler.message(payload={"command": "mailing_theme"})
+async def mailing_theme(message: Message) -> None:
     await state_dispenser.set(message.peer_id, MailingState.PENDING_THEME)
-    await message.answer("Задайте тему сообщения", keyboard=KEYBOARD_EMPTY)
+    await message.answer("Задайте тему сообщения")
 
 @mailing_labeler.message(state=MailingState.PENDING_THEME)
 async def pending_theme(message: Message) -> None:
+    await state_dispenser.delete(message.peer_id)
+    await message.answer("Тема сообщения сохранена")
+
+@mailing_labeler.message(payload={"command": "mailing_text"})
+async def mailing_text(message: Message) -> None:
     await state_dispenser.set(message.peer_id, MailingState.PENDING_TEXT)
     await message.answer("Задайте текст сообщения")
 
 @mailing_labeler.message(state=MailingState.PENDING_TEXT)
 async def pending_text(message: Message) -> None:
+    await state_dispenser.delete(message.peer_id)
+    await message.answer("Текст сообщения сохранен")
+
+@mailing_labeler.message(payload={"command": "mailing_date"})
+async def mailing_date(message: Message) -> None:
     await state_dispenser.set(message.peer_id, MailingState.PENDING_DATE)
-    await message.answer("Задайте дату отправки сообщения")
+    await message.answer("Задайте дату отправки")
 
 @mailing_labeler.message(state=MailingState.PENDING_DATE)
 async def pending_date(message: Message) -> None:
+    await state_dispenser.delete(message.peer_id)
+    await message.answer("Дата отправки сохранена")
+
+@mailing_labeler.message(payload={"command": "mailing_time"})
+async def mailing_time(message: Message) -> None:
     await state_dispenser.set(message.peer_id, MailingState.PENDING_TIME)
-    await message.answer("Задайте время отправки сообщения")
+    await message.answer("Задайте время отправки")
 
 @mailing_labeler.message(state=MailingState.PENDING_TIME)
 async def pending_time(message: Message) -> None:
     await state_dispenser.delete(message.peer_id)
+    await message.answer("Время отправки сохранено")
+
+@mailing_labeler.message(payload={"command": "mailing_done"})
+async def mailing_time(message: Message) -> None:
     await message.answer("Рассылка успешно создана!", keyboard=KEYBOARD_START)
