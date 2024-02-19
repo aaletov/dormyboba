4337bd00c41dd12422b86d0a9027efc3f5b4e7b7
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2024-02-19 20:27:00 +0300

    [ENCH-2] Use invitation link (#34)
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

--- dormyboba/__main__.py
+++ dormyboba/__main__.py
@@ -1,10 +1,10 @@
+from pathlib import Path
 from vkbottle import Bot, CtxStorage
 import grpc
-from .config import labeler, api, state_dispenser, STUB_KEY
+from .config import labeler, api, state_dispenser, STUB_KEY, CONFIG
 from .handlers import common_labeler, invite_labeler, mailing_labeler, queue_labeler, defect_labeler
 from .handlers.mailing import mailing_task
 from .handlers.queue import queue_task
-import dormyboba_api.v1api_pb2 as apiv1
 import dormyboba_api.v1api_pb2_grpc as apiv1grpc
 
 labeler.load(common_labeler)
@@ -19,16 +19,13 @@ bot = Bot(
     state_dispenser=state_dispenser,
 )
 
-channel = grpc.aio.insecure_channel('dormyboba_core:50051')
+channel = grpc.aio.insecure_channel(CONFIG.core_addr)
 stub = apiv1grpc.DormybobaCoreStub(channel)
 CtxStorage().set(STUB_KEY, stub)
 
 @bot.error_handler.register_error_handler(RuntimeError)
 async def runtime_error_handler(e: RuntimeError):
     print("Runtime error has occured", e)
-
-# @bot.loop_wrapper.interval(seconds=15)
-# async def daemons() -> None:
 
 if __name__ == "__main__":
     try:
